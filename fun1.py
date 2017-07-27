78f350f5e181117cf23729df5ffd8bad2e514eb9
mayad19-meet <mayad19@meet.mit.edu>
2017-07-27 14:04:56 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+
+
+def add_number(start,end):
+    c=0
+    for number in range(333,778):
+        print (number)
+        c=number+c
+    return c
+
+test1=add_number(1,2)
+print(test1)
+test2=add_number(1,100)
+print(test2)
+test3=add_number(1000,5000)
+print(test3)
